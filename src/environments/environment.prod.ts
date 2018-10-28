d802b6fe330bb6f653b75eea9d1e9038f02f24d0
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-10-28 15:55:29 +0300

    fix(project): fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   appName: 'VEST Tracker',
   logoUrl: '/assets/logo-black.png',
   blockchainUrl: 'http://217.23.15.22:8888',
-  chainId: '8449f44ce9ba91e7b3da01900a6c6e49cd8466ceb43352f0902f6934dbfc9f4a'
+  chainId: '8449f44ce9ba91e7b3da01900a6c6e49cd8466ceb43352f0902f6934dbfc9f4a',
+  chainParserUrl: 'http://217.23.15.22:9080'
 };
